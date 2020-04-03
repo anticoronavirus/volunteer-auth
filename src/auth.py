7469e22eee2f231207cec544a690d97f5d9b5e52
Mike Koltsov <mikekoltsov@gmail.com>
2020-04-04 01:03:12 +0300

    better naming

--- src/auth.py
+++ src/auth.py
@@ -43,15 +43,15 @@ def get_password_hash(password):
 def create_access_token(*, data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expires = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expires = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode,
                              SECRET_KEY,
                              algorithm=ALGORITHM,
                              json_encoder=UUIDEncoder)
-    return encoded_jwt, expire
+    return encoded_jwt, expires
 
 
 async def get_volunteer(phone: str):
