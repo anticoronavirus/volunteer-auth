936dec495eac741925c0c8379c2f26cfdedae13c
Mike Koltsov <mikekoltsov@gmail.com>
2020-04-03 21:42:01 +0300

    conform to new docker config

--- src/conf.py
+++ src/conf.py
@@ -1,14 +1,17 @@
 import os
+import json
 
 
 # FIXME: ENV must be preferred over local config.
 AERO_URL = "https://gate.smsaero.ru/v2"
 AERO_LOGIN = os.getenv("AERO_LOGIN", "")
 AERO_TOKEN = os.getenv("AERO_TOKEN", "")
-SECRET_KEY = os.getenv("SECRET_KEY", "")
 TOKEN_EXP_MINUTES = int(os.getenv("TOKEN_EXP_MINUTES", 3000))
 DATABASE_URL = os.getenv("DATABASE_URL", "")
-
+JWT_SETTINGS = json.loads(os.getenv("JWT_TOKEN",
+                                    '{"type": "HS256", "key": "test"}'))
+SECRET_KEY = JWT_SETTINGS["key"]
+ALGORITHM = JWT_SETTINGS["type"]
 
 try:
     from local import *
