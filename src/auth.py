9b589ad0a953880e3f4eb49ed43cb593f6b5576c
Mike Koltsov <mikekoltsov@gmail.com>
2020-04-04 01:18:53 +0300

    typo

--- src/auth.py
+++ src/auth.py
@@ -46,7 +46,7 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
         expires = datetime.utcnow() + expires_delta
     else:
         expires = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expires})
     encoded_jwt = jwt.encode(to_encode,
                              SECRET_KEY,
                              algorithm=ALGORITHM,
