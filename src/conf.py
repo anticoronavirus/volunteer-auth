0963e2293e5e7912af144fef2478722f05e29340
Mike Koltsov <mikekoltsov@gmail.com>
2020-04-03 21:35:22 +0300

    set default token live 5000 min

--- src/conf.py
+++ src/conf.py
@@ -6,7 +6,7 @@ AERO_URL = "https://gate.smsaero.ru/v2"
 AERO_LOGIN = os.getenv("AERO_LOGIN", "")
 AERO_TOKEN = os.getenv("AERO_TOKEN", "")
 SECRET_KEY = os.getenv("SECRET_KEY", "")
-TOKEN_EXP_MINUTES = int(os.getenv("TOKEN_EXP_MINUTES", 5))
+TOKEN_EXP_MINUTES = int(os.getenv("TOKEN_EXP_MINUTES", 3000))
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 
 
