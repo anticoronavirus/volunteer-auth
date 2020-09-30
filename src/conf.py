cf27c89a659ea552c4d051482b53207c6d87a082
Mike Koltsov <mikekoltsov@gmail.com>
2020-09-30 14:24:23 +0300

    rewrite conf to be idiomatic with starlette's config template

--- src/conf.py
+++ src/conf.py
@@ -1,23 +1,19 @@
-import os
 import json
+import databases
+from starlette.config import Config
 
 
-# FIXME: ENV must be preferred over local config.
-AERO_URL = "https://gate.smsaero.ru/v2"
-AERO_LOGIN = os.getenv("AERO_LOGIN", "")
-AERO_TOKEN = os.getenv("AERO_TOKEN", "")
-TOKEN_EXP_MINUTES = int(os.getenv("TOKEN_EXP_MINUTES", 15))
-REFRESH_TOKEN_EXP_MINUTES = int(os.getenv("REFRESH_TOKEN_EXP_MINUTES", 60 * 127))
-DATABASE_URL = os.getenv("DATABASE_URL", "")
-TOKEN_SCHEMA_NAME = os.getenv("TOKEN_SCHEMA_NAME", "les")
-JWT_SETTINGS = json.loads(os.getenv("JWT_SECRET",
-                                    '{"type": "HS256", "key": "test"}'))
+config = Config(".env")
+
+AERO_URL = config("AERO_URL", default="https://gate.smsaero.ru/v2")
+AERO_LOGIN = config("AERO_LOGIN", default="")
+AERO_TOKEN = config("AERO_TOKEN", default="")
+TOKEN_EXP_MINUTES = config("TOKEN_EXP_MINUTES", cast=int, default=15)
+REFRESH_TOKEN_EXP_MINUTES = config("REFRESH_TOKEN_EXP_MINUTES", cast=int, default=3) #60 * 127))
+DATABASE_URL = config("DATABASE_URL", cast=databases.DatabaseURL)
+if config("TESTING", cast=bool, default=False):
+    DATABASE_URL = DATABASE_URL.replace(database="test_" + DATABASE_URL.database)
+TOKEN_SCHEMA_NAME = config("TOKEN_SCHEMA_NAME", default="les")
+JWT_SETTINGS = config("JWT_SECRET", cast=json.loads, default='{"type": "HS256", "key": "test"}')
 SECRET_KEY = JWT_SETTINGS["key"]
 ALGORITHM = JWT_SETTINGS["type"]
-
-try:
-    from local import *
-except ImportError:
-    pass
-
-
